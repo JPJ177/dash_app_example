4e7072ffdea83e3a4782a494c4c97f65e0ff1903
JPJ177 <34447011+JPJ177@users.noreply.github.com>
2017-12-13 21:24:10 +0100

    V10

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[ ]:
+# In[1]:
 
 #importing 
 
@@ -12,11 +12,11 @@ import plotly.graph_objs as go
 import pandas as pd
 
 
-# In[ ]:
+# In[2]:
 
 #Loading the data set and splitting the years into seperate columns with sep='\t'
 
-df = pd.read_csv('Final_data.csv',sep='\t')
+df = pd.read_csv('Final_data.csv')
 
 #Check that it worked:
 df.head(5)
@@ -24,104 +24,10 @@ df.head(5)
 
 # In[ ]:
 
-#Now I am splitting up the first column which conteins the Unit, the indicator and the country or country group
+#renaming the columns for simplicity
 
-#Using split to create a new colum that contains the Units
-df['unit'] = df.iloc[:,0].str.split(',').apply(lambda x: x[0])
-
-#Same procedure for the next two: Indicator and Countries (Geo)
-df['indicator'] = df.iloc[:,0].str.split(',').apply(lambda x: x[1])
-df['geo'] = df.iloc[:,0].str.split(',').apply(lambda x: x[2])
-
-#dropping the original first column
-df = df.drop(df.columns[0], axis=1)
-
-#checking the result
-print(df.head(5))
-
-
-# In[ ]:
-
-#renaming all the units, indicators und geo locations to make the df more comprehendible
-
-df = df.replace({'unit' : {'CLV_I10': 'Chain linked volumes, index 2010=100',
-                      'CLV_I05': 'Chain linked volumes, index 2005=100',
-                      'PC_GDP': 'Percentage of gross domestic product (GDP)',
-                      'PC_EU28_MEUR_CP': 'Percentage of EU28 total (based on million euro), current prices',
-                      'PC_EU28_MPPS_CP': 'Percentage of EU28 total (based on million PPS), current prices',
-                      'CP_MEUR': 'Current prices, million euro',
-                      'CP_MNAC': 'Current prices, million units of national currency',
-                      'CP_MPPS': 'Current prices, million purchasing power standards',
-                      'CLV05_MEUR': 'Chain linked volumes (2005), million euro',
-                      'CLV05_MNAC': 'Chain linked volumes (2005), million units of national currency',
-                      'CLV10_MEUR': 'Chain linked volumes (2010), million euro',
-                      'CLV10_MNAC': 'Chain linked volumes (2010), million units of national currency',
-                      'CLV_PCH_PRE': 'Chain linked volumes, percentage change on previous period',
-                      'PYP_MNAC': 'Previous year prices, million units of national currency',
-                      'PYP_MEUR': 'Previous year prices, million euro',
-                      'CON_PPCH_PRE': 'Contribution to GDP growth, percentage point change on previous period',
-                      'PD10_NAC': 'Price index (implicit deflator), 2010=100, national currency',
-                      'PD05_NAC': 'Price index (implicit deflator), 2005=100, national currency',
-                      'PD10_EUR': 'Price index (implicit deflator), 2010=100, euro',
-                      'PD05_EUR': 'Price index (implicit deflator), 2005=100, euro',
-                      'PD_PCH_PRE_NAC': 'Price index (implicit deflator), percentage change on previous period, national currency',
-                      'PD_PCH_PRE_EUR': 'Price index (implicit deflator), percentage change on previous period, euro',
-                        }})
-
-df = df.replace({'indicator' : {'B1GQ' : 'Gross domestic product at market prices',
-                         'B1G' : 'Value added, gross',
-                         'P3' : 'Final consumption expenditure',
-                         'P3_S13': 'Final consumption expenditure of general government',
-                         'P31_S13': 'Individual consumption expenditure of general government',
-                         'P32_S13': 'Collective consumption expenditure of general government',
-                         'P31_S14_S15': 'Household and NPISH final consumption expenditure',
-                         'P31_S14': 'Final consumption expenditure of households',
-                         'P31_S15': 'Final consumption expenditure of NPISH',
-                         'P41': 'Actual individual consumption',
-                         'P5G': 'Gross capital formation',
-                         'P51G': 'Gross fixed capital formation',
-                         'P52_P53': 'Changes in inventories and acquisitions less disposals of valuables',
-                         'P52': 'Changes in inventories',
-                         'P53': 'Acquisitions less disposals of valuables',
-                         'P6': 'Exports of goods and services',
-                         'P61': 'Exports of goods',
-                         'P62': 'Exports of services',
-                         'P7': 'Imports of goods and services',
-                         'P71': 'Imports of goods',
-                         'P72': 'Imports of services',
-                         'B11': 'External balance of goods and services',
-                         'B111': 'External balance - Goods',
-                         'B112': 'External balance - Services',
-                         'D1': 'Compensation of employees',
-                         'D11': 'Wages and salaries',
-                         'D12': "Employers' social contributions",
-                         'B2A3G': 'Operating surplus and mixed income, gross',
-                         'D2X3': 'Taxes on production and imports less subsidies',
-                         'D2': 'Taxes on production and imports',
-                         'D3': 'Subsidies',
-                         'D21X31': 'Taxes less subsidies on products',
-                         'D21': 'Taxes on products',
-                         'D31': 'Subsidies on products',
-                         'YA1': 'Statistical discrepancy (production approach)',
-                         'YA0': 'Statistical discrepancy (expenditure approach)',
-                         'YA2': 'Statistical discrepancy (income approach)',
-                         'P3_P5': 'Final consumption expenditure and gross capital formation',
-                         'P3_P6':'Final consumption expenditure, gross capital formation and exports of goods and services',
-                        }})
-
-
-
-
-df = df.rename(columns=lambda x: x.strip())
-
-
-# In[ ]:
-
-#transforming the dataframe with melt
-
-
-df = pd.melt(frame=df, id_vars=['unit', 'indicator', 'geo'], value_vars=[str(i) for i in range(1975,2017)], var_name = 'year', value_name = 'value')
-df['year'] = df['year'].astype('int64')
+df=df.rename(index=str, columns={"UNIT": "unit", "GEO": "geo", "NA_ITEM": "indicator", "Value": "value", "TIME": "year"})
+df.head()
 
 
 # In[ ]:
